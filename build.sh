e0df58dd1c200613ad14b44a592a4ab39904f44c
Ethan Bennett <746393443@qq.com>
2025-09-16 09:15:54 +0800

    Update build.sh
    Signed-off-by: Ethan Bennett <746393443@qq.com>

--- build.sh
+++ build.sh
@@ -64,8 +64,8 @@ apply_config() {
         cat "$BASE_PATH/deconfig/nss.config" >> "$BASE_PATH/$BUILD_DIR/.config"
     fi
 
-    # 追加代理配置
-    cat "$BASE_PATH/deconfig/proxy.config" >> "$BASE_PATH/$BUILD_DIR/.config"
+    # # 追加代理配置
+    # cat "$BASE_PATH/deconfig/proxy.config" >> "$BASE_PATH/$BUILD_DIR/.config"
 }
 
 REPO_URL=$(read_ini_by_key "REPO_URL")
